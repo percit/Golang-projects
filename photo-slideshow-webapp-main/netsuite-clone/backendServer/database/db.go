b7516790f916c404d6b831ad405fc204ed3cc6e2
percit <marcinzdunek100@gmail.com>
2023-04-26 21:59:55 +0200

    Netsuite clone initial commit

---
+++ photo-slideshow-webapp-main/netsuite-clone/backendServer/database/db.go
@@ -0,0 +1,11 @@
+package database
+
+//tworzenie bazy danych
+
+//najpierw moze zrob te wszystkie funkcje, ale niech bierze info z jakiegos kontenerka
+//potem jak wszystko zacznie dzialac, aka routes itd, to podmien pod baze danych
+
+//instancja bazy danych
+//funkcja inicjalizujaca baze danych, aka otworz baze danych, create table
+//add/delete value i data w argumencie
+//get value z data w argumencie
---
+++ photo-slideshow-webapp-main/netsuite-clone/backendServer/server/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+//ustawiam instancje bazy danych
+//ustawiam routes
+//dodaje funkcje, wywolujace funkcje z db.go
+//ping
+//login (ale to moze)
+//autoryzacja (tak samo jak z loginem, to juz na samym koncu)
